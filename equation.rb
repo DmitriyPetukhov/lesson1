54d4fa6b6492c80739bc44a320bef965a8024e7e
Dmitrmi <61263313+Dmitrmi@users.noreply.github.com>
2020-04-16 14:24:38 +0300

    Add files via upload

--- equation.rb
+++ equation.rb
@@ -1,17 +1,18 @@
 puts "Для решения уравнения необходимо ввести  три коэффицента."
 print "Коэффицент \"а\" = :"
-a = Integer(gets.chomp)
+a = gets.to_i
 print "Коэффицент \"в\" = :"
 b = Integer(gets.chomp)
 print "Коэффицент \"с\" = :"
-c = Integer(gets.chomp)
+c = gets.to_i
 d = b**2 - 4*a*c
 if d > 0
-	x1 = (-b + Math.sqrt(d)) / (2 * a)
-	x2 = (-b - Math.sqrt(d)) / (2 * a)
+	root = Math.sqrt(d)
+	x1 = (-b + root) / (2 * a)
+	x2 = (-b - root) / (2 * a)
 	puts "D = #{d}, корень х1 = #{x1}, корень х2 = #{x2}"
 elsif d == 0
-	x1 = (-b + Math.sqrt(d))/ (2 * a)
+	x1 = -b / (2 * a)
 	puts "D = #{d}, корень х1 = #{x1}"
 elsif d < 0
 	puts "D = #{d}, КОРНЕЙ НЕТ."
